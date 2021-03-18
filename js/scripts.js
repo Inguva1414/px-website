9b09ac85251af988abe371e6fbccb95e17896cd6
Inguva1414 <65541378+Inguva1414@users.noreply.github.com>
2021-03-19 01:26:38 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,13 @@ function callglobal()  //https://share.vidyard.com/watch/9DHjPPiqZQc7czUnjorcxA?
 
 function loadContactPage() {
     aptrinsic('set', 'globalContext', { "page": "Contactpage" });
+     aptrinsic('bot', 'hide', { id: "32b72a9e-b2ab-42d8-bc65-43bea9001ee2" });
+    aptrinsic('bot', 'show', { id: "51de131e-a174-4317-ae7e-9a6df2ce0bed" }); // We can also specific ID
 }
 function loadAboutusPage() {
     aptrinsic('set', 'globalContext', { "page": "AboutusPage" });
+    aptrinsic('bot', 'hide', { id: "51de131e-a174-4317-ae7e-9a6df2ce0bed" }); 
+    aptrinsic('bot', 'show', { id: "32b72a9e-b2ab-42d8-bc65-43bea9001ee2" }); // We can also specific ID
 }
 
 function showengagement(){
