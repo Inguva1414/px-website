5d7f509589ec4480ab06352a6f9ee9f996e9f409
Inguva1414 <65541378+Inguva1414@users.noreply.github.com>
2021-03-22 17:24:10 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -31,14 +31,26 @@ function callglobal()  //https://share.vidyard.com/watch/9DHjPPiqZQc7czUnjorcxA?
 }
 
 function loadContactPage() {
-    var usermail= "demouser@gmail.com";
-    allowlogin(usermail);
+    aptrinsic('identify',
+                {
+                    //User Fields
+                    "id": "@gmai", // Required for logged in app users
+                    "email": "demouser@gmail.com"
+                    // "userHash": hash.toString()// optional transient for HMAC identification
+                },
+
+                {
+                    //Account Fields
+                    "id": "PX_Company-1", //Required
+                    "name": "PX_Company-1"
+                     // flat custom attributes
+                });
     aptrinsic('set', 'globalContext', { "page": "Contactpage" });
    //  aptrinsic('bot', 'hide', { id: "32b72a9e-b2ab-42d8-bc65-43bea9001ee2" });
    // aptrinsic('bot', 'show', { id: "51de131e-a174-4317-ae7e-9a6df2ce0bed" }); // We can also specific ID
 }
 function loadAboutusPage() {
-    var usermail= "demouser@gmail.com";
+    
     aptrinsic('identify',
                 {
                     //User Fields
